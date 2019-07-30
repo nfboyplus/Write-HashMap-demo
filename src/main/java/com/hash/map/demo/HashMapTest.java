889a2a2b545915524410a0a49c2202fed21671c6
nfboyplus <1406343123@qq.com>
2019-07-30 15:43:14 +0800

    新增知识点

--- src/main/java/com/hash/map/demo/HashMapTest.java
+++ src/main/java/com/hash/map/demo/HashMapTest.java
@@ -17,7 +17,13 @@ public class HashMapTest {
         //ddd 没有覆盖 bbb why?
         String a = (String) extMap.get("b");
         System.out.println(a);
-
     }
+
+    /**
+     * 创建一个HashMap，初始容量默认为16，负载因子默认为0.75：
+     * 1. 实际容量 = 初始容量 × 负载因子
+     * 2. 计算可知，16×0.75=12，也就是当实际容量超过12时，这个HashMap就会扩容
+     * 3. 初始容量设为不小于指定容量的2的次方的一个数（new HashMap(5),指定容量为5,那么实际初始容量为8，2^3=8>5）,且最大值不能超过2的30次方
+     */
 
 }
--- src/main/java/com/hash/map/demo/ext/ExtMap.java
+++ src/main/java/com/hash/map/demo/ext/ExtMap.java
@@ -21,4 +21,18 @@ public class ExtMap {
         return arr[key.hashCode() % arr.length];
     }
 
+    /**
+     * put()方法大概过程如下：
+     * 1. 如果添加的key值为null，那么将该键值对添加到数组索引为0的链表中，不一定是链表的首节点；
+     * 2. 如果添加的key不为null，则根据key计算数组索引的位置；
+     * 3. 数组索引处存在链表，则遍历该链表，如果发现key已经存在，那么将新的value值替换旧的value值；
+     * 4. 数组索引处不存在链表，将该key-value添加到此处，成为头节点；
+     * /
+
+     /**
+     get()方法的大概过程：
+     * 1. 如果key为null，那么在数组索引table[0]处的链表中遍历查找key为null的value；
+     * 2. 如果key不为null，根据key找到数组索引位置处的链表，遍历查找key的value，找到返回value，若没找到则返回null；
+     */
+
 }
